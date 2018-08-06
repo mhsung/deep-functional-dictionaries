6cef55e3a12f496099764c4347306a40395e08f3
Minhyuk Sung <smh0816@gmail.com>
2018-08-06 13:30:38 -0700

    Minor update.

--- network/utils/tf_ops/3d_interpolation/tf_interpolate_compile.sh
+++ network/utils/tf_ops/3d_interpolation/tf_interpolate_compile.sh
@@ -1,5 +1,13 @@
-# TF1.2
-#g++ -std=c++11 tf_interpolate.cpp -o tf_interpolate_so.so -shared -fPIC -I /usr/local/lib/python2.7/dist-packages/tensorflow/include -I /usr/local/cuda-8.0/include -lcudart -L /usr/local/cuda-8.0/lib64/ -O2 -D_GLIBCXX_USE_CXX11_ABI=0
+#!/usr/bin/env bash
+source ../config.sh
 
 # TF1.4
-g++ -std=c++11 tf_interpolate.cpp -o tf_interpolate_so.so -shared -fPIC -I /usr/local/lib/python2.7/dist-packages/tensorflow/include -I /usr/local/cuda-8.0/include -I /usr/local/lib/python2.7/dist-packages/tensorflow/include/external/nsync/public -lcudart -L /usr/local/cuda-8.0/lib64/ -L/usr/local/lib/python2.7/dist-packages/tensorflow -ltensorflow_framework -O2 -D_GLIBCXX_USE_CXX11_ABI=0
+g++ -std=c++11 tf_interpolate.cpp -o tf_interpolate_so.so -shared -fPIC \
+  -lcudart \
+  -I $cuda_include_dir \
+  -I $tensorflow_include_dir \
+  -I $tensorflow_external_dir \
+  -L $cuda_library_dir \
+  -L $tensorflow_library_dir \
+  -ltensorflow_framework -O2 -D_GLIBCXX_USE_CXX11_ABI=0
+
--- network/utils/tf_ops/sampling/tf_sampling_compile.sh
+++ network/utils/tf_ops/sampling/tf_sampling_compile.sh
@@ -1,8 +1,15 @@
-#/bin/bash
-/usr/local/cuda-8.0/bin/nvcc tf_sampling_g.cu -o tf_sampling_g.cu.o -c -O2 -DGOOGLE_CUDA=1 -x cu -Xcompiler -fPIC
+#!/usr/bin/env bash
+source ../config.sh
 
-# TF1.2
-#g++ -std=c++11 tf_sampling.cpp tf_sampling_g.cu.o -o tf_sampling_so.so -shared -fPIC -I /usr/local/lib/python2.7/dist-packages/tensorflow/include -I /usr/local/cuda-8.0/include -lcudart -L /usr/local/cuda-8.0/lib64/ -O2 -D_GLIBCXX_USE_CXX11_ABI=0
+$nvcc_bin tf_sampling_g.cu -o tf_sampling_g.cu.o -c -O2 -DGOOGLE_CUDA=1 -x cu -Xcompiler -fPIC
 
 # TF1.4
-g++ -std=c++11 tf_sampling.cpp tf_sampling_g.cu.o -o tf_sampling_so.so -shared -fPIC -I /usr/local/lib/python2.7/dist-packages/tensorflow/include -I /usr/local/cuda-8.0/include -I /usr/local/lib/python2.7/dist-packages/tensorflow/include/external/nsync/public -lcudart -L /usr/local/cuda-8.0/lib64/ -L/usr/local/lib/python2.7/dist-packages/tensorflow -ltensorflow_framework -O2 -D_GLIBCXX_USE_CXX11_ABI=0
+g++ -std=c++11 tf_sampling.cpp tf_sampling_g.cu.o -o tf_sampling_so.so -shared -fPIC \
+  -lcudart \
+  -I $cuda_include_dir \
+  -I $tensorflow_include_dir \
+  -I $tensorflow_external_dir \
+  -L $cuda_library_dir \
+  -L $tensorflow_lib \
+  -ltensorflow_framework -O2 -D_GLIBCXX_USE_CXX11_ABI=0
+
